f69fc61516a6f3b750cdf26e3f2eb27ddf742a28
contractorwolf <contractorwolf@hotmail.com>
2015-04-29 20:31:24 -0400

    added full controller functionality

--- AtariJoystickReader.ino
+++ AtariJoystickReader.ino
@@ -5,13 +5,32 @@ Connects the Atari Joystick to an Arduino (Micro)
  
 int led = 13;
 int upJoystickPin = 2;
+int downJoystickPin = 3;
+int rightJoystickPin = 4;
+int leftJoystickPin = 5;
+int buttonJoystickPin = 6;
+
+
 bool upPressed = false;
+bool downPressed = false;
+bool leftPressed = false;
+bool rightPressed = false;
+bool buttonPressed = false;
+
+
 
 
 // the setup routine runs once when you press reset:
 void setup() {                
   pinMode(led, OUTPUT);     
   pinMode(upJoystickPin, INPUT);
+  
+  pinMode(buttonJoystickPin, INPUT); 
+  pinMode(downJoystickPin, INPUT); 
+  pinMode(leftJoystickPin, INPUT);
+  pinMode(rightJoystickPin, INPUT);
+  
+  
   
   Serial.begin(9600);
   Serial.println("started");
@@ -21,9 +40,33 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   upPressed = digitalRead(upJoystickPin);
- 
-  Serial.print("pressed: ");
-  Serial.println(upPressed);
+  downPressed = digitalRead(downJoystickPin);  
+  leftPressed = digitalRead(leftJoystickPin);  
+  rightPressed = digitalRead(rightJoystickPin);  
+  buttonPressed = digitalRead(buttonJoystickPin);  
   
-  delay(20); 
+  
+  
+  
+ 
+  Serial.print("up: ");
+  Serial.print(upPressed);
+  
+  Serial.print("  down: ");
+  Serial.print(downPressed);  
+  
+  Serial.print("  left: ");
+  Serial.print(leftPressed);  
+  
+  Serial.print("  right: ");
+  Serial.print(rightPressed);  
+  
+  Serial.print("  button: ");
+  Serial.println(buttonPressed);  
+  
+  
+  
+  
+  
+  delay(2); 
 }
